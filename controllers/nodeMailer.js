668e96cedd52ce77ea78e4b960bd76434d69020f
AS21317 <ashish3553singh@gmail.com>
2024-05-08 14:45:14 +0530

    update

--- controllers/nodeMailer.js
+++ controllers/nodeMailer.js
@@ -1,27 +1,37 @@
 import nodemailer from 'nodemailer';
+import { Resend } from 'resend'
+
 import dotenv from 'dotenv';
 dotenv.config();
 
-let transporter = nodemailer.createTransport({ 
-    host:process.env.MAIL_HOST, 
-    secure: true,
-    port: 465,
-    auth:{
-        user:process.env.MAIL_USER,
-        pass:process.env.MAIL_PASS,
-    },
-});
-    
+const resend = new Resend('re_9dRvusUd_FJe2krnJYDYRBV2AJCQX3tso')
+
+// let transporter = nodemailer.createTransport({ 
+//     host:process.env.MAIL_HOST, 
+//     secure: true,
+//     port: 465,
+//     auth:{
+//         user:'resend',
+//         pass:'re_9dRvusUd_FJe2krnJYDYRBV2AJCQX3tso',
+//     },
+// });
 
 export const sendEmail = async (to, subject, html) => {
     console.log("Sending email to: ", to)
     try{
-        let info = await transporter.sendMail({
+        // let info = await transporter.sendMail({
+        //     from:process.env.MAIL_FROM,
+        //     to,
+        //     subject,
+        //     html,
+        // });
+
+        resend.emails.send({
             from:process.env.MAIL_FROM,
             to,
             subject,
             html,
-        });
+        })
         console.log(`Message sent: ${info.messageId}`);
     }catch(err){
         console.log("Error while sending email: ", err);
